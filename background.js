f3a21cab6aba79c2d570353869c6e13b08807bc7
Xavier Durand-Smet <xavier.durand-smet>
2015-08-06 11:34:42 -0400

    getting dom elements

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
 /* When the browser-action button is clicked... */
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.sendMessage(tab.id, {
-      command: "change_title",
-      title: "hoge"
-    },
-    function(msg) {
-      console.log("result message:", msg);
-    });
-});
+// chrome.browserAction.addListener(function(tab) {
+//   chrome.tabs.sendMessage(tab.id, {
+//       command: "change_title",
+//       title: "hoge"
+//     },
+//     function(msg) {
+//       console.log("result message:", msg);
+//     });
+// });
+
+// chrome.webNavigation.onCompleted.addListener(function(details) {
+//   chrome.tabs.executeScript(details.tabId, {
+//       code: "var dst = msg.title;
+//     for (var i = 0; i < document.getElementsByTagName('div').length ; i++) {
+//     	document.getElementsByTagName('div')[i].style.marginTop = Math.random()*300 + 'px';
+//     }",
+//     },
+//     function(msg) {
+//       console.log("result message:", msg);
+//     });
+// });
--- content.js
+++ content.js
@@ -1,11 +1,18 @@
-/* Listen for messages */
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  if (msg.command && (msg.command == "change_title")) {
-    var dst = msg.title;
-  	document.getElementsByTagName("div").forEach(function (elem) {
-    	 elem.innerHTML = dst;
-  	})
-    // document.getElementsByTagName("div")[0].innerHTML = dst;
-    // sendResponse("the page title's changed: '" + src + "' -> '" + dst + "'");
-  }
-});
+// /* Listen for messages */
+// chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+//   if (msg.command && (msg.command == "change_title")) {
+//     var dst = msg.title;
+//     for (var i = 0; i < document.getElementsByTagName("div").length ; i++) {
+//     	document.getElementsByTagName("div")[i].style.marginTop = Math.random()*300 + 'px';
+//     }
+//     // document.getElementsByTagName("div")[0].innerHTML = dst;
+//     sendResponse("the page title's changed");
+//   }
+// });
+
+
+
+
+    for (var i = 0; i < document.getElementsByTagName("div").length ; i++) {
+    	document.getElementsByTagName("div")[i].style.border = "thick solid blue";
+    }
